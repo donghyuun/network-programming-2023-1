3185a585b6fe846d96fb2bc0157c9fb5f89e0551
Hyun <82355395+donghyuun@users.noreply.github.com>
2023-04-20 22:34:00 +0900

    Update 주소 변환 프로그램 클라이언트.c
    refactoring

--- assignment2/주소 변환 프로그램 클라이언트.c
+++ assignment2/주소 변환 프로그램 클라이언트.c
@@ -4,90 +4,90 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-
-#define BUF_SIZE 20
-
-//result field
-#define ERROR 0
-#define SUCCESS 1
-
-//cmd field
+//cmd
 #define REQUEST 0
 #define RESPONSE 1
 #define QUIT 2
+//redsult
+#define ERROR 0
+#define SUCCESS 1
 
-//************2021428080 donghyun kim**************//
+void error_handling(char* message);
 
 typedef struct {
-    int cmd;//0: request, 1: response, 2: quit
-    char addr[BUF_SIZE];//dotted-deciaml address(20byte)
-    struct in_addr iaddr;//converted address of inet_aton()
+    int cmd; //0: request, 1: response, 2: quit
+    char addr[20]; // dotted-decimal address (20byte)
+    struct in_addr iaddr;// result of inet_aton() 
     int result;//0: Error, 1: Success
-}PACKET;
-
-void error_handling(char *message);
+} Packet;
 
 int main(int argc, char *argv[]){
-    int sock;
-    struct sockaddr_in serv_addr;
     
+    int sock; //tcp client needs just one socket
+    struct sockaddr_in serv_addr;
+
     if(argc != 3){
         printf("Usage: %s <IP> <Port>\n", argv[0]);
         exit(1);
     }
-
-    //1.generate socket
-    sock = socket(PF_INET, SOCK_STREAM, 0);//IPv4 & TCP
-    if(sock == -1) error_handling("socket() error");
     
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    serv_addr.sin_port = htons(atoi(argv[2]));
-
-    //2.conncet
-    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1){
-        error_handling("connect() error!");
+    //1. generate socket
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock == -1) {   
+        error_handling("socket() error\n");
+        return 0;   
     }
 
-    //3. read & write
-    while(1){        
-        //----send packet----
-        PACKET send_message;
-        printf("Input dotted-decimal adress: ");
-        scanf("%s", send_message.addr);
+    //initialize & set serv_addr
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;//ipv4
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);//server IP adress
+    serv_addr.sin_port = htons(atoi(argv[2]));//server Port number
 
+    //2. connect 
+    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1){
+        error_handling("connet() error\n");
+        exit(1);
+    }
+
+    while(1){
+        //generate packet & set data in packet
+        Packet send_packet;
+
+        printf("Input dotted-decimal address: ");
+        scanf("%s", send_packet.addr);
+
+        //-----send packet-----
         //quit
-        if(strcmp(send_message.addr, "quit") == 0){
-            send_message.cmd = QUIT;
-            write(sock, &send_message, sizeof(send_message));
-            printf("[Tx] cmd: %d(QUIT)\n", send_message.cmd);
+        if(strcmp(send_packet.addr, "quit") == 0){
+            send_packet.cmd = QUIT;
+            int wr_len = write(sock, &send_packet, sizeof(send_packet));
+            printf("[Tx] cmd: %d(QUIT)\n", send_packet.cmd);
             printf("Client socket close and exit\n");
-            exit(1);
+            break;
         }
-        //send
+        //not quit
+        send_packet.cmd = REQUEST;
+        printf("[Tx] cmd: %d, address: %s\n", send_packet.cmd, send_packet.addr);
+        int wr_len = write(sock, &send_packet, sizeof(send_packet));
+
+        //-----receive packet-----
+        Packet recv_packet;
+        int rd_len = read(sock, &recv_packet, sizeof(recv_packet));
+        //read error
+        if(rd_len == -1) {
+            printf("read() error\n");
+            break;
+        }
+        //convert fail
+        if(recv_packet.result == ERROR){
+            printf("[Rx] cmd: %d, address conversion fail! (result: %d)\n", recv_packet.cmd, recv_packet.result);
+        } 
+        //convert success
         else{
-            send_message.cmd = REQUEST;
-            write(sock, &send_message, sizeof(send_message));
-            printf("[Tx] cmd: %d, addr: %s\n", send_message.cmd, send_message.addr);
-
-            //receive packet
-            PACKET recv_message;
-            int len = read(sock, &recv_message, sizeof(recv_message));
-            
-            if(len == -1){
-                error_handling("client read() error\n");
-            }
-            else if(recv_message.result == 1){//receive success
-                printf("[Rx] cmd: %d, Adress converstion: %#x (result: %d)\n", recv_message.cmd, recv_message.iaddr.s_addr, recv_message.result);
-                printf("\n");
-            }
-            else{//receive fail
-                printf("[Rx] cmd: %d, Adress converstion fail! (result: %d)\n", recv_message.cmd, recv_message.result);
-                printf("\n");
-            }
+            printf("[Rx] cmd: %d, address conversion: %#x\n", recv_packet.cmd, recv_packet.iaddr.s_addr);
         }
-
+        printf("\n");
     }
 }
 
