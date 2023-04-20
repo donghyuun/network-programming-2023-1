12a8429e08652bf8ed7a3ca7e5c782c3ee734327
Hyun <82355395+donghyuun@users.noreply.github.com>
2023-04-20 22:35:06 +0900

    Update 주소 변환 프로그램 서버.c
    refactoring

--- assignment2/주소 변환 프로그램 서버.c
+++ assignment2/주소 변환 프로그램 서버.c
@@ -4,128 +4,101 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-
-#define BUF_SIZE 20
-
-//result field
-#define ERROR 0
-#define SUCCESS 1
-
-//cmd field
+//cmd
 #define REQUEST 0
 #define RESPONSE 1
 #define QUIT 2
+//redsult
+#define ERROR 0
+#define SUCCESS 1
 
-//************2021428080 donghyun kim**************//
+void error_handling(char *message);
 
 typedef struct {
-    int cmd;//0: request, 1: response, 2: quit
-    char addr[BUF_SIZE];//dotted-deciaml address(20byte)
-    struct in_addr iaddr;//converted address of inet_aton()
-    int result;//0: Error, 1: Success
-}PACKET;
+    int cmd; //0: request, 1: response, 2: quit
+    char addr[20]; // dotted-decimal address (20byte)
+    struct in_addr iaddr;// result of conversion by inet_aton() 
+    int result;//0: error, 1: success
+}Packet;
 
-void error_handling(char* message);
-
-int main(int argc, char*argv[]){
-    //int num for file descriptor
+int main(int argc, char *argv[]){
     int serv_sock;
     int clnt_sock;
 
-    //struct by "sys/socket.h" header file for IP, Port num
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in clnt_addr;
+    struct sockaddr_in serv_addr, clnt_addr;
     socklen_t clnt_addr_size;
-    
-    //packet variable for receving
-    PACKET recv_packet;//PACKET include "struct" word also.
 
     if(argc != 2){
         printf("Usage: %s <port>\n", argv[0]);
         exit(1);
     }
 
-    //1.generate socket
+    //1. socket generate
     serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-    if(serv_sock == 1) error_handling("socket() error");
-
+    if(serv_sock == -1) error_handling("socket() error\n");
+    //initialize & set serv_addr
     memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(atoi(argv[1]));
+    serv_addr.sin_family = AF_INET;//ipv4
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);//server IP adress
+    serv_addr.sin_port = htons(atoi(argv[1]));//server Port number
 
-    //2.bind address to socket
-    if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1){
-        error_handling("bind() error");
-    }else{
-        printf("--------------------------\n");
-        printf("Addreess Conversion Server\n");
-        printf("--------------------------\n");
+    //2. bind
+    if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("bind() error\n");
+    else{
+        printf("------------------------\n");
+        printf("File Transmission Server\n");
+        printf("------------------------\n");
     }
+    //3. listen
+    if(listen(serv_sock, 5) == -1)
+        error_handling("listen() error");
 
-    //3.listening
-    if(listen(serv_sock, 5) == -1) error_handling("listen() error");
-
-    //4.accept
-    clnt_addr_size = sizeof(clnt_addr);
+    //4. accept
+    clnt_addr_size = sizeof(clnt_addr_size);//important!
     clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-    if(clnt_sock == -1) error_handling("accept() error");
+    if(clnt_sock == -1) error_handling("accpet() error\n");
 
-    //5.read &  write
+    //5. read & write
     while(1){
-        int rx_len = 0;
-        rx_len = read(clnt_sock, &recv_packet, sizeof(PACKET));
-        if(rx_len == 0) break;
-
-        if(recv_packet.cmd == REQUEST){
-            printf("[Rx] Received Dotted-Decimal Address: %s\n", recv_packet.addr);
-            
-            //convertion fail
-            if(inet_aton(recv_packet.addr, &recv_packet.iaddr) == 0){
-                printf("[Tx] Address conversion fail:(%s)\n", recv_packet.addr);
-                printf("\n");
-
-                //Do i have to initialize all member of PACKET?
-                PACKET conv_error;
-                conv_error.cmd = 1;
-                conv_error.result = 0;
-
-                write(clnt_sock, &conv_error, sizeof(conv_error));
-            }
-            //convertion success
-            else{
-                printf("inet_aton(%s) -> %#x\n", recv_packet.addr, recv_packet.iaddr.s_addr);
-                
-                PACKET conv_success;
-                conv_success.cmd = 1;
-                conv_success.result = 1;
-                conv_success.iaddr = recv_packet.iaddr;
-
-                write(clnt_sock, &conv_success, sizeof(conv_success));
-                printf("[Tx] cmd: %d, iaddr: %#x, result: %d\n", conv_success.cmd, conv_success.iaddr.s_addr, conv_success.result);
-                printf("\n");
-            }
-        }
-        else if(recv_packet.cmd == QUIT){
-            printf("[Rx] QUIT message received\n");
+        //----read packet----
+        Packet recv_packet;
+        int rd_len = read(clnt_sock, &recv_packet, sizeof(recv_packet));
+    
+        //----send_packet----
+        Packet send_packet;
+        send_packet.cmd = RESPONSE;
+        //quit
+        if(recv_packet.cmd == QUIT){
+            printf("[Rx] QUIT Message received\n");
             printf("Server socket close and exit\n");
-
-            close(serv_sock);
-            close(clnt_sock);
-            exit(1);
-        }
-        else {//handling exception
-            printf("[Rx] Invalid command: %d\n", recv_packet.cmd);
-            printf("\n");
             break;
         }
+        //not quit
+        printf("[Rx] Received Dotted-Decimal Address: : %s\n", recv_packet.addr);
+        
+        //conversion fail
+        if(inet_aton(recv_packet.addr, &send_packet.iaddr) == 0){//convert addr to iaddr and store it to the right parameter
+            send_packet.result = ERROR;
+            printf("[Tx] Address conversion fail: %s\n", recv_packet.addr);
+            write(clnt_sock, &send_packet, sizeof(send_packet));
+            printf("\n");
+            continue;
+        }
+        //conversion success
+        else{
+            send_packet.result = SUCCESS;
+            printf("inet_aton(%s) -> %#x\n", recv_packet.addr, send_packet.iaddr.s_addr);
+            printf("[Tx] cmd: %d, iaddr: %#x, result: %d\n", send_packet.cmd, send_packet.iaddr.s_addr, send_packet.result);
+        }
+        int wr_len = write(clnt_sock, &send_packet, sizeof(send_packet));
+        printf("\n");
     }
     
 }
 
-//error handling
-void error_handling(char* message){
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
+void error_handling(char*message){
+    fputs(message, stderr);
+    fputc('\n',stderr);
+    exit(1);
 }
