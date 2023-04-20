5d7d58e69361d9325050f20530b9bc073f8950d8
Hyun <82355395+donghyuun@users.noreply.github.com>
2023-04-20 17:44:16 +0900

    Update hw3_client.c
    refactoring

--- assignment3/hw3_client.c
+++ assignment3/hw3_client.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <fcntl.h> //for open()
 #define BUF_SIZE 100
 #define SEQ_START 1000
 
-typedef struct{
-    int seq; //SEQ number
-    int ack; //ACK number
-    int buf_len; //File read/write bytes
-    char buf[BUF_SIZE]; //for sending file name or file content
-}Packet;
+typedef struct {
+    int seq;
+    int ack;
+    int buf_len;
+    char buf[BUF_SIZE];
+} Packet;
 
-//************2021428080 donghyun kim**************//
-void error_handling(char*message);
+void error_handling(char* message);
 
 int main(int argc, char *argv[]){
     int sock;
-    struct sockaddr_in serv_addr;
+    struct sockaddr_in serv_addr;//tcp client needs just one socket
 
     if(argc != 3){
         printf("Usage: %s <IP> <Port>\n", argv[0]);
@@ -29,79 +28,62 @@ int main(int argc, char *argv[]){
 
     //1. generate socket
     sock = socket(PF_INET, SOCK_STREAM, 0);
-    if(sock == -1) error_handling("socket() error");
+    if(sock == -1) error_handling("socket() error\n");
 
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_port = htons(atoi(argv[2]));
 
-    //2.connect 
+    //2. connect 
     if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1){
         error_handling("connect() error\n");
-        exit(1);
     }
 
-    //3. read & write
-    int fd = open("tcp.txt", O_WRONLY|O_CREAT|O_RDONLY, 0644);
+    //send filename to server
     char fileName[BUF_SIZE];
-    printf("Input file Name: ");
+    printf("Input file name: ");
     scanf("%s", fileName);
-    printf("[Client] request %s\n",fileName);
-    printf("\n");
 
-    Packet send_packet;
-    strcpy(send_packet.buf, fileName);
-    write(sock, &send_packet, sizeof(send_packet));
+    Packet name_packet;
+    strcpy(name_packet.buf, fileName);
+    write(sock, &name_packet, sizeof(name_packet));
 
+    //read & write
     int accum_len = 0;
+    int fd = open(fileName, O_RDWR|O_CREAT, 0644);
+
     while(1){
+        //read from server
         Packet recv_packet;
-        int rx_len = 0;
-        //-----receive packet-----
-        //file not exsist in server
-        rx_len = read(sock, &recv_packet, sizeof(recv_packet));
-        if(strcmp(recv_packet.buf, "File Not Found") == 0){
-            printf("%s\n", recv_packet.buf);
-            exit(1);
+        int str_len = read(sock, &recv_packet, sizeof(recv_packet));
+        
+        //File not found in server
+        if(strcmp(recv_packet.buf, "File Not Found") == 0) break;
+        
+        //File exsist in server -> write received data to file
+        printf("[Client] Rx SEQ: %d, len: %d bytes\n", recv_packet.seq, recv_packet.buf_len);
+        write(fd, recv_packet.buf, recv_packet.buf_len);
+        accum_len += recv_packet.buf_len;
+        
+        //check received data is last one
+        if(recv_packet.buf_len < 100) {//last data
+            printf("%s recevied (%d Bytes)\n", fileName, accum_len);
+            break;
         }
-        //read error
-        else if(rx_len == -1) error_handling("read() error");
-        //last data, not send packet & quit
-        else if(rx_len == 0 || recv_packet.buf_len < 100){//rx_len == 0 || rx_len < 100 NO!!!!! rx_len == 0 is correct but rx_len < 100 is not correct
-            printf("[Client] Rx SEQ: %d, len: %d bytes\n", recv_packet.seq, recv_packet.buf_len);
-            //printf("rx_len: %d, recv_pacekt.buf_len: %d, accum_len(before adding): %d\n", rx_len, recv_packet.buf_len, accum_len);
-            accum_len += recv_packet.buf_len;//rx_len NO!!!!!!
-            //printf("accum_len(after adding): %d\n", accum_len);
-            //-----write on "tcp.txt"-----
-            int final_add_len = write(fd, recv_packet.buf, recv_packet.buf_len);
-            //printf("final_add_len: %d\n", final_add_len);
-            //printf("%s sentence writed(recv_packet.buf)\n", recv_packet.buf);
-            //total received Bytes(final accum_len)
-            printf("tcp.txt received (%d Bytes)\n", accum_len);
-            close(fd); close(sock);
-            exit(1);
-        }
-        //no last data
-        else{
-            printf("[Client] Rx SEQ: %d, len: %d bytes\n", recv_packet.seq, recv_packet.buf_len);
-            //printf("rx_len: %d, recv_pacekt.buf: %d\n",rx_len, recv_packet.buf_len);
-            accum_len += recv_packet.buf_len;//rx_len NO!!!!!
-            //printf("accum_len: %d\n", accum_len);
-            //-----write on "tcp.txt"-----
-            write(fd, recv_packet.buf, sizeof(recv_packet.buf));
-            //-----send packet-----
-            Packet send_packet;
-            send_packet.ack = recv_packet.seq + 100;
-            write(sock, &send_packet, sizeof(Packet));
-            printf("[Client] Tx ACK: %d\n", send_packet.ack);
-            printf("\n");
-        }
-    }
+
+        //send ACK packet to server
+        Packet send_packet;
+        send_packet.ack = recv_packet.seq + 100;
+        write(sock, &send_packet, sizeof(send_packet));
+        printf("[Client] Tx ACK: %d\n", send_packet.ack);
+        printf("\n");
+    } 
 }
 
-void error_handling(char*message){
-    fputs(message, stderr);
-    fputc('\n',stderr);
-    exit(1);
+//error handling
+void error_handling(char* message){
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
 }
